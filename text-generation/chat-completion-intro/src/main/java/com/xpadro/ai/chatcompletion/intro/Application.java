4016ee00501411b93491023f2a436cc857c5302c
xpadro <xpadro.sw@gmail.com>
2024-05-18 11:34:41 +0200

    cleanup dependencies

--- text-generation/chat-completion-intro/src/main/java/com/xpadro/ai/chatcompletion/intro/Application.java
+++ text-generation/chat-completion-intro/src/main/java/com/xpadro/ai/chatcompletion/intro/Application.java
@@ -1,18 +1,21 @@
 package com.xpadro.ai.chatcompletion.intro;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
 		ChatService chatService = context.getBean(ChatService.class);
 		String result = chatService.chat("Tell me a joke");
-		System.out.println(result);
+		LOGGER.info(result);
 	}
 
 }
